a1170e851b424b2bdc39cef3e623d3318698dd3b
Bjorsch <johanbjork47@hotmail.com>
2016-01-07 11:27:20 +0100

    RoundNumber

--- Exercise2/FixItPage.js
+++ Exercise2/FixItPage.js
@@ -41,13 +41,13 @@ function fixSpan() {
 CreateABox(myNewFunction2);
 
 //5. PLay with the Methods 
-function CreateABox2(kalle) {
-    var myBox = document.createElement("div");
-    myBox.setAttribute("class", "box2");
-    var myDocument = document.getElementsByTagName("body");
-    myDocument[0].appendChild(myBox);
-    myBox.addEventListener('click', kalle, true);
-}
+//function CreateABox2(kalle) {
+//    var myBox = document.createElement("div");
+//    myBox.setAttribute("class", "box2");
+//    var myDocument = document.getElementsByTagName("body");
+//    myDocument[0].appendChild(myBox);
+//    myBox.addEventListener('click', kalle, true);
+//}
 
 
 function myNewFunction2() {    
@@ -60,65 +60,76 @@ function myNewFunction2() {
 //6. Play with the DOM-properties
 //### DOM page 126:###
 
-//DOM();
-//function DOM() {
-//    var msg = '<p><b>page title: </b>' + document.title + '<br/> ' ;
-//    msg += '<b>page address: </b>' +document.URL+ ' <br/>';
-//    msg += '<b>last modified : </b> ' + document.lastModified + ' </p>';
+CreateABox(DOM);
 
-//    var el = document.getElementById("two");
-//    el.innerHTML = msg;
-//}
+function DOM() {
+    var msg = '<p><b>page title: </b>' + document.title + '<br/> ' ;
+    msg += '<b>page address: </b>' +document.URL+ ' <br/>';
+    msg += '<b>last modified : </b> ' + document.lastModified + ' </p>';
+
+    var el = document.getElementById("two");
+    el.innerHTML = msg;
+}
 
 //7. Save the the text from the makeMeAnArray-paragraph into an array.
 
+var word = document.getElementById("makeMeAnArray").innerText;
+var array =  word.split(" ");
 //8. Use all the string methods and propertys allong with the array
-//name();
-//function name() {
+CreateABox(name);
+function name() {
     
-//    var makeArray = document.getElementById("makeMeAnArray").innerHTML;
-//    var msg = "Length: " + makeArray.length + "</br>";
-//    //msg += "toUpperCase: " + makeArray.toUpperCase() + "</br>";
-//    //msg += "toLowerCase: " + makeArray.toLowerCase() + "</br>";
-//    //msg += "charAt: " + makeArray.charAt(makeArray.length - 1) + "</br>";
-//    //msg += "indexOF: " + makeArray.indexOf(33) + "</br>";
-//    //msg += "lastIndexOf: " + makeArray.lastIndexOf('e') + "</br>";
-//    msg += "substring: " + makeArray.substring(0, 11) + "</br>";
-//    //msg += "split: " + makeArray.split(' ') + "</br>";
-//    //msg += "trim: " + makeArray.trim() + "</br>";
-//    //msg += "replace: " + makeArray.replace('klo', 'java') + "</br>";
+    var myArray = document.getElementById("makeMeAnArray").innerText;
+    var msg = "Length: " + myArray.length + "</br>";
+    msg += "toUpperCase: " + myArray.toUpperCase() + "</br>";
+    msg += "toLowerCase: " + myArray.toLowerCase() + "</br>";
+    msg += "charAt: " + myArray.charAt(myArray.length - 1) + "</br>";
+    msg += "indexOF: " + myArray.indexOf(33) + "</br>";
+    msg += "lastIndexOf: " + myArray.lastIndexOf('e') + "</br>";
+    msg += "substring: " + myArray.substring(0, 11) + "</br>";
+    msg += "split: " + myArray.split(' ') + "</br>";
+    msg += "trim: " + myArray.trim() + "</br>";
+    msg += "replace: " + myArray.replace('klo', 'java') + "</br>";
     
-//    var el = document.getElementById("two");
-//    el.innerHTML = msg;
+    var el = document.getElementById("two");
+    el.innerHTML = msg;
 
     
-//}
+}
 
 //### String objects page 128, 129: ###
 
 //### String objects page 132: ###
 //9. check if the 4th element in the array is a number
-function ElementInArray() {
-    var word = document.getElementById("makeMeAnArray").innerText;
-    word.split(" ");    
+CreateABox(ElementInArray);
+
+function ElementInArray() {       
     //if (word) {
     //    alert('Har ett värde!');
     //}
     //else {
     //    alert('Har INTE ett värde!');
     //}    
-    if (word[4].isNaN) {
+    if (array[4].isNaN) {
         alert("Inget Nummer");
     } else {
         alert("Nummer");
     }
 }
 
-ElementInArray();
+
 
 //### Math page 134: ###
 //10. Round one of the numbers in the paragraph up/down
+CreateABox(RoundNumber);
+function RoundNumber(parameters) {
+    alert(Math.round(array[4]));
+}
 //11. replace the 3rd word with PI then roud it to the nearest integer
+
+function ReplaceWord(parameters) {
+    
+}
 
 //### Date object###
 //12. Calculate how many days it's until your birthday and present it.
