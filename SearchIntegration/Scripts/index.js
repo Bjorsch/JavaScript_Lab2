910b22091e9e86b322aef8671f9408b50b758929
Bjorsch <johanbjork47@hotmail.com>
2016-01-08 14:38:18 +0100

    Har fixat sök

--- SearchIntegration/Scripts/index.js
+++ SearchIntegration/Scripts/index.js
@@ -1,1 +1,34 @@
-﻿
+﻿$(document).ready(function() { //Körs när htmlsidan är färdig laddad
+    SearchField();
+    $("#search").keypress(function ()
+    {
+        SearchField();
+    });
+});
+
+function SearchField() {
+   
+        var field = $("#search").val(); //sparar undan värdet man skriver in
+        var myRegex = new RegExp(field, "i");
+        $.getJSON("ItemList.json", function (data) {
+            var output = "<ul class ='result'>";
+            $.each(data, function (key, val) {
+                if ((val.name.search(myRegex) !== -1) ||
+                    (val.about.search(myRegex) !== -1)) {
+                    output += "<li>";
+                    output += "<p>";
+                    output += "Name: " + val.name + "</br>";
+                    output += " Category: " + val.Category;
+                    output += " </p>";
+                    output += "<img src='Images/" + val.url_image;
+                    output += "'alt='" + val.name;
+                    output += "'/>";
+                    output += "</li>";
+                }
+            });
+            output += "</ul>";
+            $("#update").html(output);
+        });
+}
+
+
